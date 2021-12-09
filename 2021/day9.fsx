b9c34267222a8fb0f5e5046cf538a1be18feb26e
Tomáš Pavlič <tomaspavlic@gmail.com>
2021-12-09 14:41:22 +0100

    Fix names

--- 2021/day9.fsx
+++ 2021/day9.fsx
@@ -3,32 +3,32 @@ open System.Collections.Generic
 
 let spread (x, y) = [ (x, y + 1); (x, y - 1); (x - 1, y); (x + 1, y) ]
 
-let tryGet heightmap (x, y) =
-    if x >= 0 && x < Array2D.length1 heightmap && y >= 0 && y < Array2D.length2 heightmap 
-    then Some heightmap[x, y] 
+let tryGet heightMap (x, y) =
+    if x >= 0 && x < Array2D.length1 heightMap && y >= 0 && y < Array2D.length2 heightMap 
+    then Some heightMap[x, y] 
     else None 
 
-let lowPoint heightmap (x, y) = 
-    let value = Array2D.get heightmap x y
-    if spread (x, y) |> Seq.choose (tryGet heightmap) |> Seq.forall (fun v -> v > value) then Some (x, y, value) else None
+let lowPoint heightMap (x, y) = 
+    let value = Array2D.get heightMap x y
+    if spread (x, y) |> Seq.choose (tryGet heightMap) |> Seq.forall (fun v -> v > value) then Some (x, y, value) else None
 
-let basin heightmap lowPoint =
+let basin heightMap lowPoint =
     let visited = HashSet()
     let rec inner coord =  
         visited.Add(coord) |> ignore
         spread coord
-        |> List.filter (visited.Contains >> not)
-        |> List.filter (tryGet heightmap >> Option.map (fun v -> v <> 9uy) >> Option.defaultValue false)
-        |> List.iter inner
+        |> Seq.filter (visited.Contains >> not)
+        |> Seq.filter (tryGet heightMap >> Option.map (fun v -> v <> 9uy) >> Option.defaultValue false)
+        |> Seq.iter inner
     inner lowPoint
     visited.Count + 1
 
-let heightmap = File.ReadAllLines("inputs/day9_input.txt") |> Seq.map (Seq.map (fun c -> uint8 c - uint8 '0')) |> array2D
+let heightMap = File.ReadAllLines("inputs/day9_input.txt") |> Seq.map (Seq.map (fun c -> uint8 c - uint8 '0')) |> array2D
 
 let lowPoints = 
-    [ for x in 0 .. Array2D.length1 heightmap - 1 do
-        for y in 0 .. Array2D.length2 heightmap - 1 do x, y ] 
-    |> List.choose (lowPoint heightmap)
+    [ for x in 0 .. Array2D.length1 heightMap - 1 do
+        for y in 0 .. Array2D.length2 heightMap - 1 do x, y ] 
+    |> List.choose (lowPoint heightMap)
 
 let part1 = lowPoints |> List.map (fun (_, _, v) -> int v + 1) |> List.reduce (+)
-let part2 = lowPoints |> List.map (fun (x, y, _) -> basin heightmap (x, y)) |> List.sortDescending |> List.take 3 |> List.reduce (*)
+let part2 = lowPoints |> List.map (fun (x, y, _) -> basin heightMap (x, y)) |> List.sortDescending |> List.take 3 |> List.reduce (*)
